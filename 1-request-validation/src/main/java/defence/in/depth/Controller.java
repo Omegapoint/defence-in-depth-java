cb7ef29e81f777c4441b133f802cdccb64b2b2f4
Erica Löfström <lofstrom.erica@gmail.com>
2022-05-03 06:12:10 +0200

    add demo 1 - request validation
    Most important here is the Dockerfile and nginx.conf

---
+++ 1-request-validation/src/main/java/defence/in/depth/Controller.java
@@ -0,0 +1,13 @@
+package defence.in.depth;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+}
