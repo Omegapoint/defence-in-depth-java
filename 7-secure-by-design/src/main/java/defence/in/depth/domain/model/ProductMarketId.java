8fa14cd329339ef20147b6f92c661a5cf5973aac
Erica Löfström <lofstrom.erica@gmail.com>
2023-01-18 21:10:39 +0100

    Update ProductMarket domain object

--- 7-secure-by-design/src/main/java/defence/in/depth/domain/model/ProductMarketId.java
+++ 7-secure-by-design/src/main/java/defence/in/depth/domain/model/ProductMarketId.java
@@ -1,7 +1,32 @@
 package defence.in.depth.domain.model;
 
+import defence.in.depth.domain.exceptions.InvalidDomainPrimitiveException;
+
+import java.util.Arrays;
+
 public enum ProductMarketId {
     SE,
     NO,
     FI;
+
+    public static boolean isValidId(String id) {
+        return Arrays.stream(values())
+                .anyMatch(marketId -> marketId.name().equalsIgnoreCase(id));
+    }
+
+    public static void assertValidId(String id) {
+        if (!isValidId(id)) {
+            throw new InvalidDomainPrimitiveException("Unable to parse product id");
+        }
+    }
+
+    public static ProductMarketId of(String market) {
+        assertValidId(market);
+        for (ProductMarketId marketId : ProductMarketId.values()) {
+            if (marketId.name().equalsIgnoreCase(market)) {
+                return marketId;
+            }
+        }
+        throw new InvalidDomainPrimitiveException("Unable to parse product id");
+    }
 }
