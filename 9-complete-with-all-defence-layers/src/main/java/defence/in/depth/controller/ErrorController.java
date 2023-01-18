4fa9d3d04fd00fa836e31fab958301ead7a25812
Erica Löfström <lofstrom.erica@gmail.com>
2023-01-18 21:52:32 +0100

    Update controllers and domain logic

---
+++ 9-complete-with-all-defence-layers/src/main/java/defence/in/depth/controller/ErrorController.java
@@ -0,0 +1,20 @@
+package defence.in.depth.controller;
+
+import org.apache.commons.lang3.NotImplementedException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/error")
+public class ErrorController {
+
+    @PutMapping("")
+    @PreAuthorize("permitAll()")
+    public ResponseEntity throwException() {
+        throw new NotImplementedException("Not implemented on purpose");
+    }
+
+}
