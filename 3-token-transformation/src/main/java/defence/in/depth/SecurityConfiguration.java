12d50ee28478368c3959bc22a22ea4ef5642fd98
Erica Edholm <erica@edholm.dev>
2024-09-01 22:30:00 +0200

    Update spring-boot-parent to 3.3.3

--- 3-token-transformation/src/main/java/defence/in/depth/SecurityConfiguration.java
+++ 3-token-transformation/src/main/java/defence/in/depth/SecurityConfiguration.java
@@ -26,7 +26,9 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests(authorize -> authorize
                         .anyRequest().authenticated()
                 )
-                .oauth2ResourceServer(jwt -> jwt.jwt().jwtAuthenticationConverter(new CustomJwtAuthenticationConverter()));
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(
+                        jwt -> jwt.jwtAuthenticationConverter(new CustomJwtAuthenticationConverter())
+                ));
         return http.build();
     }
 
